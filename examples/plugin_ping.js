666d2f46b9f0794025cd274a02f106cf703eb253
Yonah Russ <yonah.russ@winner.com>
2014-04-30 12:49:09 +0300

    Refactored the code to be less asynchronous and avoid running out of sockets.

--- examples/plugin_ping.js
+++ examples/plugin_ping.js
@@ -1,41 +1,51 @@
-// This plugin requires installing the net-ping module before it can be used.
 ping = require("net-ping");
 var options = {
   retries: 1,
-  timeout: 2000
+  timeout: 500
 };
 
 var pingInterval = setInterval(function () {
 // tests object sets ip addresses to ping
 // all remote addresses must be alive for the vippy node
 // to remain active.
-  var tests = {
-    "192.168.1.1": "10.112.102.125"
-  }
-  for (testIp in tests) {
+
+  var tests = ["192.168.1.4","192.168.1.1","192.168.1.2"],
+  i = 0,
+  up=true;
+
+  function pingit() {
+    if (!up || i >= tests.length){
+        config.active(up, "pingRemoteFailed");
+        config.log('debug', config.inactive_reason());
+        return;
+    }
+    var testIp = tests[i];
     var session = ping.createSession(options);
     session.on("error", function (error) {
       config.log('err', error.toString());
     });
+
     session.pingHost(testIp, function (error, target) {
       if (error) {
         if (error instanceof ping.RequestTimedOutError) {
-          config.log('err', target + ": Down");
+          config.log('crit', target + ": Down");
         } else {
-          config.log('err', target + ": " + error.toString());
+          config.log('crit', target + ": " + error.toString());
         }
-        config.active(false, "pingRemoteFailed");
-        config.log('debug', config.inactive_reason());
+        up=false;
+        config.log('debug', "var up = "+up.toString())
       } else {
         config.log('debug', target + ": Up");
-        config.active(true, "pingRemoteFailed");
-        config.log('debug', config.inactive_reason());
+        i++;
       }
-    });
+    pingit();
     session.close();
+    });
   }
-}, 1000);
+  pingit();
+}, 3000);
 config.on('stop', function () {
   // clearIntervals
   clearInterval(pingInterval);
-});#
+  config.active(false,"shutting down");
+});
